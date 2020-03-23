e7ecfc493b6485a8e37c4ebe381e9caebb996cbf
duraes-antonio <garotoseis@gmail.com>
2020-03-23 04:31:15 -0300

    FIX: Teste em CI 11

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,13 @@
 if (!process.env.NODE_ENV) {
-  process.env.NODE_ENV = 'test';
+  process.env.NODE_ENV = "test";
 }
 
 module.exports = {
-  moduleFileExtensions: ['js', 'ts'],
-  testResultsProcessor: 'jest-sonar-reporter',
-  transform: {'^.+\\.ts$': 'ts-jest'},
-  testMatch: ['<rootDir>/tests/**/*.test.ts'],
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transformIgnorePatterns: ["/node_modules/**"],
-  globals: {
-    'ts-jest': {
-      tsConfig: "tsconfig.json",
-      diagnostics: false
-    }
-  }
+  moduleFileExtensions: ["js", "ts"],
+  testResultsProcessor: "jest-sonar-reporter",
+  transform: { "^.+\\.ts$": "ts-jest" },
+  testMatch: ["<rootDir>/tests/**/*.test.ts"],
+  preset: "ts-jest",
+  testEnvironment: "node"
 };
+
