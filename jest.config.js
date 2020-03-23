6e88368c6dd41b3ed0202ec425c616de8e1d167e
duraes-antonio <garotoseis@gmail.com>
2020-03-23 04:01:33 -0300

    FIX: Teste em CI 8

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,18 @@
-// if (!process.env.NODE_ENV) {
-//   process.env.NODE_ENV = 'test';
-// }
-//
-// module.exports = {
-//   moduleFileExtensions: ['js', 'ts'],
-//   testResultsProcessor: 'jest-sonar-reporter',
-//   transform: {'^.+\\.ts$': 'ts-jest'},
-//   testMatch: ['<rootDir>/tests/**/*.test.ts'],
-//   preset: 'ts-jest',
-//   testEnvironment: 'node',
-//   globals: {
-//     'ts-jest': {
-//       diagnostics: false
-//     }
-//   }
-// };
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = 'test';
+}
+
+module.exports = {
+  moduleFileExtensions: ['js', 'ts'],
+  testResultsProcessor: 'jest-sonar-reporter',
+  transform: {'^.+\\.ts$': 'ts-jest'},
+  testMatch: ['**/tests/*.+(ts|tsx|js)'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsConfigFile: "tsconfig.json",
+      diagnostics: false
+    }
+  }
+};
